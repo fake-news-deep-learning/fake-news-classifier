179ba1364a88d4ddcb1f0b6c9314950976c23a0e
Guilherme Perrotta <g173846@dac.unicamp.br>
2020-12-12 03:10:25 -0300

    Add script to split politifact by sample label

---
+++ split.py
@@ -0,0 +1,54 @@
+import json
+
+from sklearn.model_selection import train_test_split
+
+
+def main():
+    with open('data/processed/politifact.json', 'r') as input_file:
+        politifact = json.load(input_file)
+    print(f'politifact: {len(politifact)} samples')
+
+    fake = 0
+    real = 0
+    stratify = []
+
+    dataset = list(politifact.values())
+    for sample in dataset:
+
+        if sample['label'] == 'fake':
+            fake += 1
+        else:
+            real += 1
+
+        stratify.append(sample['label'])
+
+    print(f'{fake} fake samples')
+    print(f'{real} real samples')
+
+    # first, splitting into trainval and test
+    trainvalid, test = train_test_split(
+        dataset, test_size=0.2, stratify=stratify)
+    print(f'Train/Valid: {len(trainvalid)}; Test: {len(test)}')
+
+    # then, splitting trainval into train and valid
+    stratify = [sample['label'] for sample in trainvalid]
+    train, valid = train_test_split(
+        trainvalid, test_size=0.15, stratify=stratify)
+    print(f'Train: {len(train)}; Valid: {len(valid)}')
+
+    # serializing split sets
+    train = {sample['id']: sample for sample in train}
+    with open('data/processed/train.json', 'w') as out_file:
+        json.dump(train, out_file, sort_keys=True, indent=2)
+
+    valid = {sample['id']: sample for sample in valid}
+    with open('data/processed/valid.json', 'w') as out_file:
+        json.dump(valid, out_file, sort_keys=True, indent=2)
+
+    test = {sample['id']: sample for sample in test}
+    with open('data/processed/test.json', 'w') as out_file:
+        json.dump(test, out_file, sort_keys=True, indent=2)
+
+
+if __name__ == '__main__':
+    main()
