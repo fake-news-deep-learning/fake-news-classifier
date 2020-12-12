90480525e2c11216fde6511de0085b9d11265261
Luiz Cartolano <duducartolano@gmail.com>
2020-12-12 19:23:20 -0300

    add function to create callbacks

--- model_utils.py
+++ model_utils.py
@@ -1,4 +1,5 @@
-from keras import metrics
+import os
+from keras import metrics, callbacks
 
 
 metrics_dict = {
@@ -14,6 +15,19 @@ metrics_dict = {
     'FalseNegatives': metrics.FalseNegatives()
 }
 
+
+callbacks_dict = {
+    'ModelCheckpoint': callbacks.ModelCheckpoint(filepath='checkpoints/model.{epoch:02d}-{val_loss:.2f}.h5',
+                                                 monitor='val_loss', verbose=1),
+    'TensorBoard': callbacks.TensorBoard(log_dir='logs', update_freq='epoch', embeddings_freq=0),
+    'EarlyStopping': callbacks.EarlyStopping(monitor='val_loss', patience=5, verbose=1,
+                                             restore_best_weights=False),
+    'ReduceLROnPlateau': callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=5,
+                                                     verbose=1),
+    'TerminateOnNaN': callbacks.TerminateOnNaN()
+}
+
+
 def create_metrics(metrics_names):
     """
         :param metrics_names: list of metrics names
@@ -26,3 +40,26 @@ def create_metrics(metrics_names):
         array_metrics.append(metrics_dict[metric])
 
     return array_metrics
+
+
+def create_callbacks(callbacks_names=None):
+    """
+        :param callbacks_names:
+
+        :return:
+    """
+    if not os.path.exists('logs'):
+        os.makedirs('logs')
+
+    if not os.path.exists('checkpoints'):
+        os.makedirs('checkpoints')
+
+    if not callbacks_names:
+        callbacks_names = ['ModelCheckpoint', 'TensorBoard', 'EarlyStopping']
+
+    array_callbacks = []
+
+    for callback in callbacks_names:
+        array_callbacks.append(callbacks_dict[callback])
+
+    return array_callbacks
