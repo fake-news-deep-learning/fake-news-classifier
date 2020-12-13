a30e953fbc938b050fb8f1034414e0cbd418ee6b
Guilherme Perrotta <g173846@dac.unicamp.br>
2020-12-13 18:48:01 -0300

    Add docstrings to model.py

--- src/model.py
+++ src/model.py
@@ -1,30 +1,28 @@
-import tensorflow
-from tensorflow.python import keras
+from typing import List, Tuple
 
+from tensorflow.python import keras
+from keras import Model, layers
 from keras.models import Sequential
-from keras import layers
+
 from model_utils import create_metrics
 
 
-def create_model(num_filters, input_shape):
-    """
-        :param num_filters:
-        :param vocab_size:
-        :param embedding_dim:
-        :param maxlen:
-
-        :return:
-    """
+def create_text_cnn(input_shape: Tuple) -> Model:
+    """Implements the TextCNN architecture using given input shape."""
+    filters_per_layer = 36
     embed_size = input_shape[1]
+
     model = Sequential()
     model.add(layers.InputLayer(input_shape))
+
     for kernel_size in [1, 2, 3, 5]:
         model.add(layers.Conv2D(
-            num_filters,
+            filters_per_layer,
             (kernel_size, embed_size),
             padding='same',
             name=f'conv2d_kernel_{kernel_size}_layer'),
         )
+
     model.add(layers.Dropout(0.1, name='dropout_layer'))
     model.add(layers.Flatten())
     model.add(layers.Dense(1, name='linear_layer', activation='sigmoid'))
@@ -32,15 +30,13 @@ def create_model(num_filters, input_shape):
     return model
 
 
-def compile_model(model_to_compile, metrics_names):
-    """
-        :param model_to_compile:
-        :param metrics_names:
+def compile_model(model_to_compile: Model, metrics_names: List[str]) -> Model:
+    """Compiles a Model using given metrics names."""
 
-        :return:
-    """
-    model_to_compile.compile(optimizer='adam',
-                             loss='binary_crossentropy',
-                             metrics=create_metrics(metrics_names))
+    model_to_compile.compile(
+        optimizer='adam',
+        loss='binary_crossentropy',
+        metrics=create_metrics(metrics_names),
+    )
 
     return model_to_compile
