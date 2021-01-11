3bb543f8088e4c0976c7f8afdee25235a95d5c1c
Guilherme Perrotta <g173846@dac.unicamp.br>
2021-01-10 22:28:34 -0300

    Add util to remove short samples

--- remove_empty.py
+++ remove_empty.py
@@ -1,20 +1,50 @@
 import json
+import re
 
-for label in ['train', 'test', 'valid']:
 
-    with open(f'data/processed/{label}.json', 'r') as input:
-        dataset = json.load(input)
+def filter_empty():
+    for label in ['train', 'test', 'valid']:
 
-    to_delete = []
-    for id in dataset:
-        entry = dataset[id]
-        if len(entry['text']) < 10:
-            to_delete.append(id)
+        with open(f'data/processed/{label}.json', 'r') as input:
+            dataset = json.load(input)
 
-    for id in to_delete:
-        del dataset[id]
+        to_delete = []
+        for id in dataset:
+            entry = dataset[id]
+            if len(entry['text']) < 10:
+                to_delete.append(id)
 
-    print(f'Removed {len(to_delete)} entries from {label}')
+        for id in to_delete:
+            del dataset[id]
 
-    with open(f'data/processed/{label}.json', 'w') as output:
-        json.dump(dataset, output, sort_keys=True, indent=2)
+        print(f'Removed {len(to_delete)} entries from {label}')
+
+        with open(f'data/processed/{label}.json', 'w') as output:
+            json.dump(dataset, output, sort_keys=True, indent=2)
+
+
+def filter_short_samples(minimum_size=60):
+
+    for label in ['train', 'test', 'valid']:
+
+        with open(f'data/processed/{label}.json', 'r') as input:
+            dataset = json.load(input)
+
+        to_delete = []
+        for id in dataset:
+
+            entry = dataset[id]
+            text = entry['text']
+            text = re.sub(r'\n+', ' ', text).split()
+
+            if len(text) < minimum_size:
+                to_delete.append(id)
+
+        for id in to_delete:
+            del dataset[id]
+
+        print(f'Removed {len(to_delete)} entries from {label}')
+
+        with open(f'data/processed/{label}.json', 'w') as output:
+            print(f'{label} size after processing: {len(dataset)}')
+            json.dump(dataset, output, sort_keys=True, indent=2)
