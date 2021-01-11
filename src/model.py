ce87499b4ded634d29426676f1956f25509d0453
Luiz Cartolano <duducartolano@gmail.com>
2021-01-11 19:50:19 -0300

    teste 7

--- src/model.py
+++ src/model.py
@@ -37,14 +37,14 @@ def create_lstm_model(input_shape: Tuple) -> Model:
 
         :return:
     """
-    embedding_dim = input_shape[1]
+    # embedding_dim = input_shape[1]
 
     model = Sequential()
-    model.add(layers.InputLayer(input_shape[1], input_shape[2]))
+    # model.add(layers.InputLayer(input_shape[1], input_shape[2]))
     model.add(layers.SpatialDropout1D(0.5))
     # model.add(layers.LSTM(100, dropout=0.1, recurrent_dropout=0.2))
-    model.add(layers.LSTM(64, return_sequences=True))
-    model.add(layers.LSTM(32, return_sequences=False))
+    model.add(layers.LSTM(64, input_shape=(300), return_sequences=True))
+    model.add(layers.LSTM(32, input_shape=(300), return_sequences=False))
     model.add(layers.Dense(1, activation='sigmoid', name='linear_layer'))
 
     return model
