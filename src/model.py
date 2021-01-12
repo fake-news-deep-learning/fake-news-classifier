1161c9a50a4eb14da5474eddef6440e2211d1eaa
Luiz Cartolano <duducartolano@gmail.com>
2021-01-11 23:25:31 -0300

    add input shape, last try, falhei

--- src/model.py
+++ src/model.py
@@ -44,9 +44,9 @@ def create_lstm_model(input_shape: Tuple) -> Model:
 
     # model.add(layers.InputLayer(input_shape=(70, )))
 
-    model.add(layers.Bidirectional(layers.LSTM(256, 
-                                               input_shape=(300, 70), 
-                                               return_sequences=False)))
+    model.add(layers.Bidirectional(layers.LSTM(256,  
+                                               return_sequences=False))
+                                    input_shape=(300, 70))
 
     # model.add(layers.Flatten())
     model.add(layers.Dense(1,
